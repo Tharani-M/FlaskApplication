bf0b5fa5a2bf0b85e80b9cc356e49626971099fb
Tharani-M <tharanimoorthy07@gmail.com>
2024-07-02 15:51:22 +0530

    change

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request
 from flask import abort
 from markupsafe import escape
+import json
 from db import user,books
 
 app = Flask(__name__)
@@ -18,8 +19,7 @@ def getusers():
         request_data = request.get_json()
         
         user.append(request_data)
-        for u in user:
-              return u
+        return json.dumps(user,indent=2)
     
 @app.route('/<email>',methods=["GET","POST"])
 def postusers(email):
@@ -29,7 +29,8 @@ def postusers(email):
           for u in user:
                if u['email'] == email:
                
-                    return u
+                    return json.dumps(u,indent=2)
+
           else:
                abort(404,message="Data is not available")
      if request.method == "POST":
@@ -37,7 +38,7 @@ def postusers(email):
                if u['email'] == email:
                     u =  req
                     user += req
-                    return u
+                    return json.dumps(u,indent=2)
           else:
                abort(404,message="Data is not available")
 
@@ -48,7 +49,7 @@ def updateuser(email):
     for u in user:
                if u['email'] == email:
                     u.update(req)                  
-                    return user
+                    return json.dumps(user,indent=2)
     else:
          abort(404,message="Data is not available")
 
@@ -58,7 +59,7 @@ def deluser(email):
     for u in user:
                if u['email'] == email:                  
                     user.remove(u)
-                    return user
+                    return json.dumps(user,indent=2)
     else:
                abort(404,"Data is not available") 
         
@@ -68,7 +69,8 @@ def getuser(email):
     # return f'User {escape(email)}'
     # for i in user:ö
     if user['email'] == email:
-            return user['usertype']
+            return json.dumps(user['usertype'],indent=2)
+    
     
 # GET,POST,PUT,DELETE requests in books
 
@@ -76,11 +78,11 @@ def getuser(email):
 def getsetbooks():
       global books
       if request.method == "GET":
-            return books
+            return json.dumps(books,indent=2)
       if request.method == "POST":
             req_book = request.get_json()
             books.append(req_book)
-            return books
+            return json.dumps(books,indent=2)
 
 @app.route('/api/books/<int:book_id>',methods=['GET','POST'])
 def specificbooks(book_id):
@@ -88,13 +90,13 @@ def specificbooks(book_id):
       if request.method == 'GET':
              for b in books:
                    if b['book_id'] == book_id:
-                         return b
+                         return json.dumps(b,indent=2)
       if request.method == 'POST':
             req_book_up = request.get_json()
             for b in books:
                if b['book_id'] == book_id: 
                         b.update(req_book_up)
-                        return  books
+                        return  json.dumps(books,indent=2)
 
 @app.put('/api/books/<int:bookid>')
 def updatebookinfo(bookid):
@@ -103,7 +105,7 @@ def updatebookinfo(bookid):
                if b['book_id'] == bookid: 
                      book_info_up = request.get_json()
                      b.update(book_info_up)
-                     return  books
+                     return  json.dumps(books,indent=2)
 
 @app.delete('/api/books/<int:bookid>')
 def deletebookinfo(bookid):
@@ -111,7 +113,7 @@ def deletebookinfo(bookid):
       for b in books:
                if b['book_id'] == bookid: 
                      books.remove(b)
-                     return  books
+                     return  json.dumps(books,indent=2)
 
 
     
